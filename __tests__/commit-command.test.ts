6e34b33c6747c004bb9e9891981f46590480d587
twinh <twinhuang@qq.com>
2019-08-30 23:43:31 +0800

    refactor: Use lerna to publish subdirectory as packages #5

---
+++ __tests__/commit-command.test.ts
@@ -0,0 +1,85 @@
+import * as fs from 'fs';
+import {createRepo, removeRepos, disableColor, runCommand, catchError} from '../../../packages/sync/__tests__/tester';
+import commit from '../index';
+
+beforeAll(() => {
+  disableColor();
+});
+
+afterAll(() => {
+  removeRepos();
+});
+
+describe('commit command', () => {
+  test('run commit', async () => {
+    const source = await createRepo();
+    const target = await createRepo();
+
+    await source.addFile('.git-sync.json', JSON.stringify({
+      repos: [
+        {
+          dir: 'package-name',
+          remote: target.dir,
+        }
+      ]
+    }));
+
+    await source.commitFile('package-name/test.txt');
+
+    await runCommand(commit, source, {
+      dir: 'package-name',
+    });
+
+    expect(fs.existsSync(target.getFile('test.txt'))).toBeTruthy();
+  });
+
+  test('config file does not exist', async () => {
+    const source = await createRepo();
+
+    const error = await catchError(async () => {
+      await runCommand(commit, source, {
+        dir: 'package-name',
+      });
+    });
+
+    expect(error).toEqual(new Error('Config file ".git-sync.json" does not exist.'));
+  });
+
+  test('config dir not found', async () => {
+    const source = await createRepo();
+
+    await source.addFile('.git-sync.json', '{}');
+
+    const error = await catchError(async () => {
+      await runCommand(commit, source, {
+        dir: 'package-name',
+      });
+    });
+
+    expect(error).toEqual(new Error('Directory "package-name" does not exist in config file.'));
+  });
+
+  test('repository dir not found', async () => {
+    const source = await createRepo();
+    const target = await createRepo();
+
+    await source.addFile('.git-sync.json', JSON.stringify({
+      repos: [
+        {
+          dir: 'package-name',
+          remote: target.dir,
+        }
+      ]
+    }));
+
+    const error = await catchError(async () => {
+      await runCommand(commit, source, {
+        dir: 'package-name',
+      });
+    });
+
+    expect(error).toEqual(new Error('Directory "package-name" does not exist in current repository.'));
+  });
+});
+
+
---
+++ index.ts
@@ -0,0 +1,37 @@
+import {Arguments, CommandModule} from 'yargs';
+import Sync from '@gitsync/sync';
+import {Config} from '@gitsync/config';
+
+interface CommitArguments extends Arguments {
+  dir: string
+}
+
+let command: CommandModule = {
+  handler: () => {
+  }
+};
+
+command.command = 'commit <dir>';
+
+command.describe = 'Sync the commits from the current repository\'s subdirectory to another repository';
+
+command.builder = {
+  dir: {
+    describe: 'The subdirectory in current repository',
+  }
+};
+
+command.handler = (argv: CommitArguments) => {
+  const config = new Config();
+  const target: string = config.getRemoteByDir(argv.dir);
+
+  const sync = new Sync();
+  return sync.sync({
+    $0: '',
+    _: [],
+    target: target,
+    sourcePath: argv.dir,
+  });
+}
+
+export default command;
